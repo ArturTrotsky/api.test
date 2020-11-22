d06f9da98f9dc06d92cfaa222509526d9066b00f
Artur Trotsky <artur.trotsky@ukr.net>
2020-11-22 14:55:49 +0200

    ref

--- register.php
+++ register.php
@@ -2,8 +2,8 @@
 
 // Create new record in users.json
 $ch = curl_init();
-$user = ["login" => "test000000000000000000000000000000000000", "password" => "123"];
+$user = ["login" => "tes112ttest", "password" => "123"];
 curl_setopt($ch, CURLOPT_POSTFIELDS, $user);
-curl_setopt($ch, CURLOPT_URL, 'http://beetroot/homework15/register.php');
+curl_setopt($ch, CURLOPT_URL, 'http://beetroot:8081/homework15/register.php');
 $output = curl_exec($ch);
 curl_close($ch);
